7dfb024c3893483130b9001dafa15225b273b085
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-04-02 17:10:23 -0600

    nuevo soporte

--- software/testing.py
+++ software/testing.py
@@ -2,4 +2,21 @@ from rubik_solver import utils
 
 cube = "wowgybwyogygybyoggrowbrgywrborwggybrbwororbwborgowryby"
 
-utils.solve(cube, 'Beginner')
+print(utils.solve(cube, 'Kociemba'))
+utils.pprint(cube)
+res = ""
+for i in cube:
+    if i == 'w':
+        res += 'D'
+    if i == 'o':
+        res += 'B'
+    if i == 'g':
+        res += 'R'
+    if i == 'r':
+        res += 'F'
+    if i == 'b':
+        res += 'L'
+    if i == 'y':
+        res += 'U'
+
+print(res)
