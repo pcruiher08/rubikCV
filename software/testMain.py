ef064d2ea22c1cd2af937eec4f8031def8e8603e
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-05-28 01:27:28 -0500

    olasod

--- software/testMain.py
+++ software/testMain.py
@@ -4,7 +4,7 @@ from motor import Motor
 F = Motor(26,19)
 R = Motor(13,6)
 B = Motor(21, 20)
-'''
+
 F.setDelay(.005 / 8 * 2)
 F.turn90CW()
 F.turn90CW()
@@ -12,7 +12,7 @@ F.turn90CW()
 F.turn90CCW()
 F.turn90CCW()
 F.turn90CCW()
-'''
+
 
 R.setDelay(.005 / 8 * 2)
 R.turn90CW()
