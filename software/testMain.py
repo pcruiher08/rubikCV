d594542bb6a72672d03c839c32d5bd2ba32a26d1
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-05-28 01:09:33 -0500

    2nd motor

--- software/testMain.py
+++ software/testMain.py
@@ -1,39 +1,48 @@
 from motor import Motor
 
 
-D = Motor(26,19)
+R = Motor(26,19)
+B = Motor(13,6)
 '''
 L = Motor(12,16)
 
-D.setDelay(.005 / 8 * 4)
+R.setDelay(.005 / 8 * 4)
 L.setDelay(.005 / 8 * 4)
 
 
-D.turn90CW()
+R.turn90CW()
 L.turn90CW()
 
-D.turn90CW()
+R.turn90CW()
 L.turn90CW()
 
-D.turn90CW()
+R.turn90CW()
 L.turn90CW()
 
-D.turn90CCW()
+R.turn90CCW()
 L.turn90CCW()
 
-D.turn90CCW()
+R.turn90CCW()
 L.turn90CCW()
 
-D.turn90CCW()
+R.turn90CCW()
 L.turn90CCW()
 '''
 
 
-D.setDelay(.005 / 8 * 4)
-D.turn90CW()
-D.turn90CW()
-D.turn90CW()
-D.turn90CCW()
-D.turn90CCW()
-D.turn90CCW()
+R.setDelay(.005 / 8 * 4)
+R.turn90CW()
+R.turn90CW()
+R.turn90CW()
+R.turn90CCW()
+R.turn90CCW()
+R.turn90CCW()
 
+
+B.setDelay(.005 / 8 * 4)
+B.turn90CW()
+B.turn90CW()
+B.turn90CW()
+B.turn90CCW()
+B.turn90CCW()
+B.turn90CCW()
