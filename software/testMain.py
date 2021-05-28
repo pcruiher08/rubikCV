bb573e2c56a067df40590b4b186d8330fb6adec2
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-05-28 01:22:20 -0500

    second motor test

--- software/testMain.py
+++ software/testMain.py
@@ -4,7 +4,7 @@ from motor import Motor
 F = Motor(26,19)
 R = Motor(13,6)
 B = Motor(21, 20)
-
+'''
 F.setDelay(.005 / 8 * 2)
 F.turn90CW()
 F.turn90CW()
@@ -12,8 +12,8 @@ F.turn90CW()
 F.turn90CCW()
 F.turn90CCW()
 F.turn90CCW()
-
 '''
+
 R.setDelay(.005 / 8 * 2)
 R.turn90CW()
 R.turn90CW()
@@ -23,7 +23,7 @@ R.turn90CCW()
 R.turn90CCW()
 
 
-
+'''
 B.setDelay(.005 / 8 * 2)
 B.turn90CW()
 B.turn90CW()
