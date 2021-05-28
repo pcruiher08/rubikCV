6c34cefe3294f8131e782125d4ac17f6b8e3d345
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-05-28 01:43:35 -0500

    back

--- software/testMain.py
+++ software/testMain.py
@@ -2,8 +2,10 @@ from motor import Motor
 
 
 F = Motor(26,19)
-R = Motor(13,6)
-B = Motor(21, 20)
+L = Motor(13,6)
+R = Motor(21, 20)
+B = Motor(16,12)
+
 
 F.setDelay(.005 / 8 * 1)
 F.turn90CW()
@@ -12,6 +14,15 @@ F.turn90CW()
 F.turn90CCW()
 F.turn90CCW()
 F.turn90CCW()
+
+
+L.setDelay(.005 / 8 * 1)
+L.turn90CW()
+L.turn90CW()
+L.turn90CW()
+L.turn90CCW()
+L.turn90CCW()
+L.turn90CCW()
 
 
 R.setDelay(.005 / 8 * 1)
