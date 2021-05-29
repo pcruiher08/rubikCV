5b0826328db393cb87128cb37684ec0b6e84af6c
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-05-29 18:20:50 -0500

    punto

--- software/cuadrantDetection.py
+++ software/cuadrantDetection.py
@@ -86,7 +86,7 @@ while True:
             P20 = (P5[0] + distancePointPoint(bottomCorner, rightCorner) - 10, P5[1] + distancePointPoint(P5, P4) - 13)
             P19 = getFractionPoint(P5, P20, 2/3 + 0.08)
             P21 = (P1[0] + distancePointPoint(P4,P5)-7, P1[1] + 16)
-            P22 = getFractionPoint(P21, P8, 1/3+0.05)
+            P22 = getFractionPoint(P21, P8, 1/3+0.10)
             P25 = (P10[0]-35, P1[1]+20)
             P24 = getFractionPoint(P25, P3, 1/3)
 
