41c2f60f066daf047c22b5b449cbc358f451582e
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-05-31 13:12:34 -0500

    led test

--- software/cuadrantDetection.py
+++ software/cuadrantDetection.py
@@ -3,6 +3,8 @@ import cv2.aruco as aruco
 import numpy as np
 import os
 import math as m 
+import board
+import neopixel
 
 def rgb_to_hsv(r, g, b):
     r, g, b = r/255.0, g/255.0, b/255.0
@@ -133,12 +135,59 @@ cap = cv2.VideoCapture(0)
 greenColor = (0,255,0)
 blueColor = (255,0,0)
 redColor = (0,0,255)
+# On CircuitPlayground Express, and boards with built in status NeoPixel -> board.NEOPIXEL
+# Otherwise choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D1
+pixel_pin = board.NEOPIXEL
+num_pixels = 10
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+
 while True:
     success, img = cap.read()
     frame = img.copy()
     arucofound = findArucoMarkers(img)
     howManyArucos = len(arucofound[0])
-
+    pixels.fill((255, 0, 0))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((255, 0, 0, 0))
+    pixels.show()
     if howManyArucos!=0:
         coordinates = []
         for bbox, id in zip(arucofound[0], arucofound[1]):
