e3c59c2905a43738fb109c4eace8828457e40750
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-05-31 16:49:27 -0500

    led test

--- software/LEDS.ino
+++ software/LEDS.ino
@@ -8,12 +8,13 @@ CRGB leds[NUM_LEDS];
 void setup() {
 
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-
+  FastLED.setBrightness(5);
 
 }
 
 void loop() {
   for (int i = 0; i <= NUM_LEDS; i++) {
+    if(i != 20 && i != 19 && i != 18 && i != 7)
     leds[i] = CRGB ( 255, 255, 255);
     FastLED.show();
     delay(40);
