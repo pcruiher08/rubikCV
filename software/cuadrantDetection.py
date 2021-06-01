9dc5fbd8d3f0bc071e674f2eb88225cfc7097727
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-06-01 00:46:41 -0500

    test con leds

--- software/cuadrantDetection.py
+++ software/cuadrantDetection.py
@@ -74,7 +74,14 @@ def getAverageInsidePolygon(img, polygon):
     mask = np.zeros(img.shape[:2], dtype = np.uint8)
     cv2.fillPoly(mask, pts = [polygon], color = (255,255,255))
     average = cv2.mean(imghsv,mask=mask)[:3]
-    
+    color_ranges_HSV = [
+    [(55, 0, 161),"W"],
+    [(13, 53, 255),"O"],
+    [(127, 58, 131),"G"],
+    [(8, 70, 131),"R"],
+    [(60, 47, 189),"Y"],
+    [(209, 90, 255),"B"]]
+    '''
     color_ranges_HSV = [
     [(55, 0, 161), (210, 17, 255),"W"],
     [(13, 53, 255), (43, 68, 255),"O"],
@@ -82,7 +89,7 @@ def getAverageInsidePolygon(img, polygon):
     [(8, 70, 131), (17, 100, 250),"R"],
     [(60, 47, 189), (66, 65, 255),"Y"],
     [(55, 0, 161), (210, 17, 255),"B"]]
-    
+    '''
     '''
     color_ranges_HSV = [
     [(180, 18, 255), (0, 0, 231),"W"],
@@ -98,11 +105,12 @@ def getAverageInsidePolygon(img, polygon):
     minimumDistance = 1000
     for color in color_ranges_HSV:
         comparison1 = calculateHSVDistance(color[0], average)
-        comparison2 = calculateHSVDistance(color[1], average)
-        closestColor = min(comparison1, comparison2)
+        #comparison2 = calculateHSVDistance(color[1], average)
+        #closestColor = min(comparison1, comparison2)
+        closestColor = comparison1
         if( closestColor < minimumDistance ):
             minimumDistance = closestColor
-            nearestColor = color[2]
+            nearestColor = color[1]
 
     if(nearestColor == 'W'):
         average = (255,255,255)
