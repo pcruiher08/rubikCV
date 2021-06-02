d6407aafa4fbdb494d49e515baefe605b6e0d700
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-06-01 21:26:31 -0500

    poligono c1

--- software/cuadrantDetection.py
+++ software/cuadrantDetection.py
@@ -153,8 +153,8 @@ def findArucoMarkers(img, markerSize = 4, totalMarkers=250, draw=True):
 def drawPolygons(img, polygons):
     for polygon in polygons:
         for i in range(len(polygon)):
-            img = cv2.circle(img, [polygon[i]], 2, blueColor, 5)        
-            img = cv2.line(img, [polygon[i]], [polygon[i % 4]],redColor,2)
+            img = cv2.circle(img, tuple(polygon[i]), 2, blueColor, 5)        
+            img = cv2.line(img, tuple(polygon[i]), tuple(polygon[i % 4]),redColor,2)
 
 
 cv2.namedWindow('mouseRGB')
