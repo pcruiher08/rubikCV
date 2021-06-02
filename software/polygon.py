9f48d90af406f9d2f871a3662874b7406c848142
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-06-02 00:03:02 -0500

    poligono c1

--- software/polygon.py
+++ software/polygon.py
@@ -38,7 +38,7 @@ class Polygon:
         print("cambiando color")
         self.colorIndex = (self.colorIndex + 1) % 6
         self.color = self.colors[self.colorIndex][0]
-        self.colorLetter = self.colors[self.colorIndex][1]
+        self.colorLetter = 'K'
         self.color = (255,0,0)
     def setCoordinates(self, coordinates):
         self.coordinates = coordinates
