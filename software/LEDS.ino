16c12b78e2ffd998a944ae0d7696d156f5d70206
Pablo C. Ruíz <pcruiher089@gmail.com>
2021-06-04 17:55:40 -0500

    leds?

--- software/LEDS.ino
+++ software/LEDS.ino
@@ -8,14 +8,18 @@ CRGB leds[NUM_LEDS];
 void setup() {
 
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(5);
+  FastLED.setBrightness(4);
 
 }
 
 void loop() {
   for (int i = 0; i <= NUM_LEDS; i++) {
-    if(i != 20 && i != 19 && i != 18 && i != 7)
-    leds[i] = CRGB ( 255, 255, 255);
+    //if(i!= 21 && i != 20 && i != 19 && i != 18 && i != 7 )
+    if(i > 7 && i < 20){
+      if(i > 15) FastLED.setBrightness(6);
+      if(i != 11 && i!= 12 && i!= 13)
+      leds[i] = CRGB ( 255, 255, 255);
+    }
     FastLED.show();
     delay(40);
   }
